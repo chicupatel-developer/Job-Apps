423b158ed246a2ce18fb998f58c03f325c306723
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2021-11-30 14:55:14 -0600

    follow-up-wip2

--- Angular/src/app/components/follow-up/follow-up.component.ts
+++ Angular/src/app/components/follow-up/follow-up.component.ts
@@ -14,6 +14,10 @@ export class FollowUpComponent implements OnInit {
 
   jobApps = [];
 
+  filterForm: FormGroup;
+  provinceCollection: any = ['MB', 'ON', 'AB'];
+  cityCollection: string[] = [];
+
   constructor(
     private router: Router,
     public dataService: DataService,
@@ -22,7 +26,52 @@ export class FollowUpComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+      this.filterForm = this.formBuilder.group({    
+        city: [''],
+        province: [''],
+        contactPersonName: ['']
+      });
+    
     this.getAllJobApps();
+  }
+  
+  changeProvince(e) {
+    // reset city, when province gets changed
+    this.cityCollection = [];
+    this.filterForm.controls['city'].setValue('');
+
+    if (e.target.value == "") {
+      return;
+    }
+    else {
+      var cities = this.localDataService.getCities(e.target.value);
+      this.cityCollection = cities;
+    }
+  }
+
+  filterNow() {
+   
+    var filterProvince = this.filterForm.value["province"];
+    var filterCity = this.filterForm.value["city"];
+    var filterContactPersonName = this.filterForm.value["contactPersonName"];
+    var jobApps_ = this.jobApps;
+    if (this.filterForm.value["province"] != '') {
+      jobApps_ = jobApps_.filter(function (job) {
+        return job.province === filterProvince;
+      });
+    }
+    if (this.filterForm.value["city"] != '') {
+      jobApps_ = jobApps_.filter(function (job) {
+        return job.city === filterCity;
+      });
+    }
+    if (this.filterForm.value["contactPersonName"] != '') {
+      jobApps_ = jobApps_.filter(function (job) {
+        return job.contactPersonName === filterContactPersonName;
+      });
+    }
+    this.jobApps = jobApps_;
+    // console.log(jobApps_);
   }
 
   getAllJobApps() {
