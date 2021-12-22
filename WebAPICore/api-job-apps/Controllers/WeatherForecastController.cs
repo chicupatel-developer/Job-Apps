9029ec2e0f036f924c83e516d98d71c0080ed4a4
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2021-12-22 16:04:19 -0600

    resume-html-to-pdf-wip2

--- WebAPICore/api-job-apps/Controllers/WeatherForecastController.cs
+++ WebAPICore/api-job-apps/Controllers/WeatherForecastController.cs
@@ -5,6 +5,7 @@ using SelectPdf;
 using Services.DTO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -45,21 +46,93 @@ namespace api_job_apps.Controllers
         [Route("htmltopdf")]
         public IActionResult HtmlToPdf()
         {
+            // prepare data for resume
+            UserProfile user = new UserProfile();        
+
             // instantiate a html to pdf converter object
             HtmlToPdf converter = new HtmlToPdf();
 
             // set converter options
             converter.Options.PdfPageSize = PdfPageSize.A4;
             converter.Options.PdfPageOrientation = PdfPageOrientation.Portrait;
-            converter.Options.WebPageWidth = 600;
-            converter.Options.WebPageHeight = 1024;
+            converter.Options.WebPageWidth = 1000;
+            converter.Options.WebPageHeight = 1414;
+            var content = @"<html>
+                             <head> 
+                                <style>
+                                    body {
+    
+}
 
-            var pdf = converter.ConvertUrl("https://www.roundthecode.com/");
+.nameDiv{
+            text-align: center;
+            vertical-align: middle;
+            padding: 5px;
+            border: 1px solid blueviolet;
+            border-radius: 10px;
+            background-color: lightseagreen;
+            color:black;
+            font-size: x-large;  
+            width: 300px;
+}
+                                </style>
+                             </head>
+                             <body>
+                                <div class='nameDiv'>
+                                    Ankit Patel
+                                </div>
+                             </body>
+                            </html>
+                            ";
+
+            // var pdf = converter.ConvertUrl("https://www.roundthecode.com/");           
+            // var pdf = converter.ConvertHtmlString(content);
+            
+            var pdf = converter.ConvertHtmlString(user.GetResume());
+            
             var pdfBytes = pdf.Save();
             return File(pdfBytes, "application/pdf");
         }
+    }  
 
+    class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetResume()
+        {
+            FirstName = "Ankit";
+            LastName = "Patel";
+
+            string content = @"<html>
+                             <head> 
+                                <style>
+                                    body {   
+                                    }
+                                    .nameDiv{
+                                                text-align: center;
+                                                vertical-align: middle;
+                                                padding: 5px;
+                                                border: 1px solid blueviolet;
+                                                border-radius: 10px;
+                                                background-color: lightseagreen;
+                                                color:black;
+                                                font-size: x-large;  
+                                                width: 300px;
+                                    }
+                                </style>
+                             </head>
+                             <body>
+                                <div class='nameDiv'>"+
+                                    FirstName + LastName + 
+                                    @"                                    
+                                </div>
+                             </body>
+                            </html>
+                            ";
+
+            return content;
+        }
     }
-   
-  
 }
